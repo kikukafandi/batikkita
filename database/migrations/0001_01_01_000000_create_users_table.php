38e59b9dabbfb155887f901c0bbc053dae961a33
kikukafandi <jtierta@gmail.com>
2025-09-08 11:32:44 +0700

    feat:setup data migration and add controller

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role',['buyer','seller','admin'])->default('buyer');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
